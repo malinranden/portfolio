080037ea7e0939342caf5637465b389341d73d2d
Malin Randén <rantatalo94@gmail.com>
2026-01-05 14:45:43 +0100

    basic structure for all content parts

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -6,11 +6,19 @@ function Portfolio () {
         <> 
             <Navbar></Navbar>
             <div className='container-portolio-page'>
-                <div className='welcome-container'>
+                <div className='container-welcome'>
                     <h1>Hi, my name is Malin</h1>
                     <img src="" alt="image-of-me" />
                 </div>
-                <div></div>
+                <div className='container-about-me'>
+                    <h3>About me</h3>
+                </div>
+                <div className='container-my-projects'>
+                    <h3>My projects</h3>
+                </div>
+                <div className='container-contact-me'>
+                    <h3>Contact me form</h3>
+                </div>
             </div>
         </>
     )
