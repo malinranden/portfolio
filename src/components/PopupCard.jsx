d6c6f7035d7b5a717bce8c54fbedcd80f4fec72a
Malin Randén <rantatalo94@gmail.com>
2026-01-15 15:41:31 +0100

    styling popupcard

--- src/components/PopupCard.jsx
+++ src/components/PopupCard.jsx
@@ -5,17 +5,26 @@ function PopupCard({popupContent, onClose}) {
     
     return (
         <div className="PopupCard" onClick={onClose}>
-            <div className="PopupCard__info" onClick={(e) => e.stopPropagation()}>
-                <button onClick={onClose} className="PopupCard__closeBtn">x</button>
-                <h2>{popupContent.title}</h2>
-                {/* <img src={popupContent.img} alt={popupContent.title} /> */}
-                <p>{popupContent.popupInfo.shortWriteUp}</p>
-                <p>{popupContent.popupInfo.info}</p>
-                <p>Here you can see the <a href={popupContent.popupInfo.linkHost}>result</a></p>
-                <p>Here is the <a href={popupContent.popupInfo.linkRepo}>link</a> to the repository</p>
-                {/* <a href={popupContent.popupInfo.link}>
-                    View GitHub Repo
-                </a> */}
+            <div className="PopupCard__container">
+                <button onClick={onClose} className="PopupCard__closeBtn"><i className="fa-solid fa-x"></i></button>
+                <div className="PopupCard__image-section">
+                    <img className="PopupCard__img" src={popupContent.popupInfo.images} alt="images" />
+                </div>
+                <div className="PopupCard__text-section" onClick={(e) => e.stopPropagation()}>
+                    {/* <div className="PopupCard__info" > */}
+                        
+                        <h2>{popupContent.title}</h2>
+                        {/* <img src={popupContent.img} alt={popupContent.title} /> */}
+                        <p>{popupContent.popupInfo.shortWriteUp}</p>
+                        <p>{popupContent.popupInfo.info}</p>
+                        <p>Here you can see the <a href={popupContent.popupInfo.linkHost}>result</a></p>
+                        <p>Here is the <a href={popupContent.popupInfo.linkRepo}>link</a> to the repository</p>
+                        {/* <a href={popupContent.popupInfo.link}>
+                            View GitHub Repo
+                        </a> */}
+                    {/* </div> */}
+                </div>
+                
             </div>
         </div> 
     )
--- src/components/Projectinfo.jsx
+++ src/components/Projectinfo.jsx
@@ -2,7 +2,8 @@ import Brhomaon from "../assets/brhomaon.png"; // ./ eller ../ ?
 // import genesis from "../assets/genesis.png";
 import portfolio from '../assets/portfolio.png'
 import game from '../assets/game.png'
-
+import gamegame from '../assets/gamegame.png'
+import gamesignin from '../assets/gamesignin.png'
 const Projectinfo = [ 
     {
         id: 1,
@@ -11,7 +12,7 @@ const Projectinfo = [
         info: "info",
         tags: ["#CSS", " ", "#Javascript", " ", "#Sass"],
         popupInfo: {
-            shortWriteUp: "This was a test in the course where we had 12 hours to complete this Figma file. Besides from creating the image we had to add interactivity and a responsive design for all devices. This was an assignment to create a webpage from a Figma form.",
+            shortWriteUp: "A 12 hours deadline test to create this Figma file, adding interactivity and responsive design for all screen-sizes.",
             info: "info",
             linkHost: "https://github.com/malinranden/test-your-skills.git",
             linkRepo: "https://github.com/malinranden/test-your-skills.git",
@@ -42,6 +43,7 @@ const Projectinfo = [
             linkHost: "#",
             linkRepo: "#",
             // link: "#",
+            images: gamesignin
         }
     }
 ]
